5ce1a5aa6261e8102705b74d1cd571aa44e61d25
Sayan <sayanlev@gmail.com>
2024-12-22 23:40:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('arr' + ' - object');
+console.log(4 + '5');
+
+let incr = 10;
+let decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log(2 + 2 * 2 !== 6);
+
+/* &&
+|| */
+
+const isCheked = false,
+      isClose = false;
+      console.log(isCheked || !isClose);
